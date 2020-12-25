08af5b1551df1f68d0ea87edaefc6f92e7a94551
Chisato Murakami <kiwipom1031@gmail.com>
2020-12-25 14:35:29 -0800

    add styled component in ChooseDialect

---
+++ App.js
@@ -0,0 +1,6 @@
+import ChooseDialect from "./src/ChooseDialect";
+import React from "react";
+
+export default function App() {
+  return <ChooseDialect />;
+}
--- src/ChooseDialect.js
+++ src/ChooseDialect.js
@@ -1,16 +1,27 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from "react";
+import styled from "styled-components/native";
 
-export const ChooseDialect = (props) => {
+export const ChooseDialect = () => {
   return (
-    <>
-      
-    </>
-  )
-}
+    <SafeareaContainer>
+      <Container>
+        <ContainerText>Hello</ContainerText>
+      </Container>
+    </SafeareaContainer>
+  );
+};
 
-const mapStateToProps = (state) => ({
-  
-})
+const SafeareaContainer = styled.SafeAreaView`
+  flex: 1;
+  flex-direction: row;
+  justify-content: center;
+  background-color: #7fc8f8;
+`;
+const Container = styled.SafeAreaView`
+  height: 100%;
+`;
+const ContainerText = styled.Text`
+  text-align: center;
+`;
 
-export default connect(mapStateToProps, mapDispatchToProps)(ChooseDialect)
+export default ChooseDialect;
