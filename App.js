81b634c8eb4fe0325a7a0d3bbff708a7840d255d
gabe <gabe.p.dominguez@gmail.com>
2020-12-26 12:40:25 -0800

    Installs react-navigation

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
+// import "react-native-gesture-handler";
 import ChooseDialect from "./src/ChooseDialect";
 import React from "react";
 import styled from "styled-components/native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <SafeareaContainer>
-      <ChooseDialect />
-    </SafeareaContainer>
+    <NavigationContainer>
+      <SafeareaContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ChooseDialect" component={ChooseDialect} />
+        </Stack.Navigator>
+      </SafeareaContainer>
+    </NavigationContainer>
   );
 }
 
