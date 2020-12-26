9aafb38b3a63f2a3170bb85eed9634082ac55181
gabe <gabe.p.dominguez@gmail.com>
2020-12-26 13:24:17 -0800

    Moved color into constant so easier to change

--- App.js
+++ App.js
@@ -7,12 +7,19 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
-
+const BACKGROUND_COLOR = "#7fc8f8";
 export default function App() {
   return (
     <NavigationContainer>
       <SafeareaContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+            cardStyle: {
+              backgroundColor: BACKGROUND_COLOR,
+            },
+          }}
+        >
           <Stack.Screen name="ChooseDialect" component={ChooseDialect} />
           <Stack.Screen name="Lesson" component={Lesson} />
         </Stack.Navigator>
@@ -24,5 +31,5 @@ export default function App() {
 const SafeareaContainer = styled.SafeAreaView`
   flex: 1;
   flex-direction: row;
-  background-color: #7fc8f8;
+  background-color: ${BACKGROUND_COLOR};
 `;
