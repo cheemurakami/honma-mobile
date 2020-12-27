dc00d306cdbcd003da3365069ea12db37fc7d0c7
gabe <gabe.p.dominguez@gmail.com>
2020-12-26 19:31:27 -0800

    Adds icons to project

--- src/Lesson.js
+++ src/Lesson.js
@@ -1,10 +1,18 @@
-import React from "react";
 import { Text, View } from "react-native";
 
-const Lesson = () => {
+import Icon from "react-native-vector-icons/FontAwesome";
+import React from "react";
+import { Title } from 'react-native-paper';
+
+const Lesson = ({ navigation }) => {
   return (
     <View>
-      <Text>Lesson Screen</Text>
+      <Icon
+        name="arrow-left"
+        size={30}
+        onPress={() => navigation.navigate("ChooseDialect")}
+      />
+      <Title style={{textAlign: 'center'}}>いる / おる　おるん?</Title>
     </View>
   );
 };
