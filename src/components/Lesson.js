76db0a861a5e94de5fd8778baae2548948ec4b95
gabe <gabe.p.dominguez@gmail.com>
2020-12-26 20:26:33 -0800

    Adds more styling to the lesson page

--- src/components/Lesson.js
+++ src/components/Lesson.js
@@ -1,13 +1,13 @@
 import { Text, View } from "react-native";
 
-import FooterButton from "../shared/FooterButton"
+import FooterButton from "../shared/FooterButton";
 import Icon from "react-native-vector-icons/FontAwesome";
 import React from "react";
-import { Title } from 'react-native-paper';
+import { Title } from "react-native-paper";
 import styled from "styled-components/native";
 
-const pageTitle = "いる / おる　おるん?"
-const btnTitle = "完了"
+const pageTitle = "いる / おる　おるん?";
+const btnTitle = "完了";
 const Lesson = ({ navigation }) => {
   return (
     <View>
@@ -16,10 +16,21 @@ const Lesson = ({ navigation }) => {
         size={30}
         onPress={() => navigation.navigate("ChooseDialect")}
       />
-      <Title style={{textAlign: 'center'}}>{pageTitle}</Title>
-      <BodyContainer></BodyContainer>
+      <Title style={{ textAlign: "center" }}>{pageTitle}</Title>
+      <BodyContainer>
+        <MediaContainer></MediaContainer>
+        <BodyText>いる changes to おる </BodyText>
+        <BodyText>おるん for questions</BodyText>
+        <ExampleContainer>
+          <BodyTextExample>Q: 今どこにおるん？</BodyTextExample>
+          <BodyTextExample>A: 広島駅におるよ</BodyTextExample>
+          <BodyTextExample>Q: Where are you now?</BodyTextExample>
+          <BodyTextExample>A: I'm at Hiroshima Station.</BodyTextExample>
+        </ExampleContainer>
+      </BodyContainer>
+
       <Footer>
-        <FooterButton title={btnTitle}/>
+        <FooterButton title={btnTitle} />
       </Footer>
     </View>
   );
@@ -35,4 +46,26 @@ const Footer = styled.View`
   align-items: center;
 `;
 
-export default Lesson
+const BodyText = styled.Text`
+  text-align: center;
+  font-size: 20px;
+  margin: 10px;
+  font-weight: bold;
+`;
+
+const BodyTextExample = styled.Text`
+  text-align: left;
+  font-size: 20px;
+  margin: 10px;
+`;
+
+const MediaContainer = styled.View`
+  height: 40%;
+`;
+
+const ExampleContainer = styled.View`
+  padding: 10px;
+  margin-top: 30px;
+`;
+
+export default Lesson;
--- src/shared/FooterButton.js
+++ src/shared/FooterButton.js
@@ -7,8 +7,8 @@ const FooterButton = ({ title, onPressHandler }) => {
       mode="contained"
       onPress={onPressHandler}
       color="#FFE45E"
-      style={{width: '90%', height: 45, justifyContent: 'center'}}
-      labelStyle={{fontSize: 25}}
+      style={{ width: "90%", height: 45, justifyContent: "center" }}
+      labelStyle={{ fontSize: 25 }}
     >
       {title}
     </Button>
