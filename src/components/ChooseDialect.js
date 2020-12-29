f844b90941872f434613d26583086ce1692b9653
Chisato Murakami <kiwipom1031@gmail.com>
2020-12-28 21:16:53 -0800

    add layout and navigation in ChooseDialect

--- src/components/ChooseDialect.js
+++ src/components/ChooseDialect.js
@@ -1,56 +1,72 @@
-import { Button, List } from "react-native-paper";
-
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { List } from "react-native-paper";
 import React from "react";
+import ScreenLayout from "../shared/ScreenLayout";
 import styled from "styled-components/native";
 
+const pageTitle = "Choose your dialect";
+const btnLabel = "はじめるで";
+const dialects = [
+  {
+    name: "広島弁",
+    en: "Hiroshima",
+  },
+  {
+    name: "大阪弁",
+    en: "Osaka",
+  },
+  {
+    name: "京都弁",
+    en: "Kyoto",
+  },
+];
+
 const ChooseDialect = ({ navigation }) => {
-  const dialects = [
-    {
-      name: "広島弁",
-      en: "Hiroshima",
-    },
-    {
-      name: "大阪弁",
-      en: "Osaka",
-    },
-    {
-      name: "京都弁",
-      en: "Kyoto",
-    },
-  ];
-
   return (
-    <Container>
-      <ContainerText>Choose your dialect</ContainerText>
-
-      {dialects.map((dialect) => {
-        return (
-          <List.Item
-            key={dialect.en}
-            title={dialect.name + " " + dialect.en}
-            style={{ width: "100%" }}
-            left={(props) => <List.Icon {...props} icon="folder" />}
-          />
-        );
-      })}
-      <Button
-        mode="contained"
-        onPress={() => navigation.navigate("PatternList")}
-      >
-        Select pattern
-      </Button>
-    </Container>
+    <ScreenLayout
+      pageTitle={pageTitle}
+      btnLabel={btnLabel}
+      onPressHandler={() => navigation.navigate("PatternList")}
+    >
+      <DialectContainer>
+        {dialects.map((dialect, index) => {
+          return (
+            <DialectTouchable
+              key={index}
+              onPress={() => navigation.navigate("PatternList")}
+            >
+              <List.Item
+                title={dialect.name + " " + dialect.en}
+                titleNumberOfLines={2}
+                titleStyle={{ fontSize: 20 }}
+                style={{ width: "100%" }}
+                left={() => (
+                  <Icon
+                    name="flower-poppy"
+                    size={30}
+                    style={{ margin: 10, color: "#aacc00" }}
+                  />
+                )}
+              />
+            </DialectTouchable>
+          );
+        })}
+      </DialectContainer>
+    </ScreenLayout>
   );
 };
 
-const Container = styled.SafeAreaView`
+const DialectContainer = styled.View`
   flex: 1;
   justify-content: center;
-  align-items: center;
-  height: 100%;
+  padding: 10px;
 `;
-const ContainerText = styled.Text`
-  text-align: center;
+
+const DialectTouchable = styled.TouchableHighlight.attrs({
+  underlayColor: "#7fc8f8",
+})`
+  margin-top: 10px;
+  border-radius: 25px;
 `;
 
 export default ChooseDialect;
