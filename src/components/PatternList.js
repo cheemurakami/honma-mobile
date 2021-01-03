dce48817bf4ef9002f24ca5a699148108025ad13
Chisato Murakami <kiwipom1031@gmail.com>
2021-01-02 20:35:15 -0800

    add ScrollView in PatternList

--- src/components/PatternList.js
+++ src/components/PatternList.js
@@ -2,6 +2,7 @@ import Icon from "react-native-vector-icons/FontAwesome";
 import { List } from "react-native-paper";
 import React from "react";
 import ScreenLayout from "../shared/ScreenLayout";
+import { ScrollView } from "react-native";
 import styled from "styled-components/native";
 
 const pageTitle = "Choose your patterns";
@@ -16,34 +17,36 @@ export const PatternList = ({ route, navigation }) => {
       backComponentName={"ChooseDialect"}
       onPressHandler={() => navigation.navigate("Lesson")}
     >
-      <PatternContainer>
-        {grammars.map((grammar, index) => {
-          return (
-            <ListTouchable
-              key={index}
-              onPress={() =>
-                navigation.navigate("Lesson", {
-                  grammar,
-                })
-              }
-            >
-              <List.Item
-                title={grammar.label}
-                titleNumberOfLines={2}
-                titleStyle={{ fontSize: 20 }}
-                style={{ width: "100%" }}
-                left={() => (
-                  <Icon
-                    name="leaf"
-                    size={30}
-                    style={{ margin: 10, color: "#aacc00" }}
-                  />
-                )}
-              />
-            </ListTouchable>
-          );
-        })}
-      </PatternContainer>
+      <ScrollView>
+        <PatternContainer>
+          {grammars.map((grammar, index) => {
+            return (
+              <ListTouchable
+                key={index}
+                onPress={() =>
+                  navigation.navigate("Lesson", {
+                    grammar,
+                  })
+                }
+              >
+                <List.Item
+                  title={grammar.label}
+                  titleNumberOfLines={2}
+                  titleStyle={{ fontSize: 20 }}
+                  style={{ width: "100%" }}
+                  left={() => (
+                    <Icon
+                      name="leaf"
+                      size={30}
+                      style={{ margin: 10, color: "#aacc00" }}
+                    />
+                  )}
+                />
+              </ListTouchable>
+            );
+          })}
+        </PatternContainer>
+      </ScrollView>
     </ScreenLayout>
   );
 };
