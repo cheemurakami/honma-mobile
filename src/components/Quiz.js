7d3d6079fac142b3f66d796b52d7774ee17ef477
Chisato Murakami <kiwipom1031@gmail.com>
2021-01-28 16:16:36 -0800

    chenge button color and text depends on inputted answer.

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,14 +1,32 @@
 import React, { useState } from "react";
 
+import { Alert } from "react-native";
 import { Button } from "react-native-paper";
 import { TextInput } from "react-native-paper";
 import styled from "styled-components/native";
 
 export const Quiz = ({ selectedDialect, grammar }) => {
   const [text, setText] = useState("");
+  const [correctAnswer, setCorrectAnswer] = useState("");
+  const [buttonText, setButtonText] = useState("答え合わせ");
+  const [buttonColor, setButtonColor] = useState("#40BA62");
 
   if (grammar.quizzes.length > 0) {
     const quizTokyo = grammar.quizzes[0].tokyo;
+    const answer = grammar.quizzes[0].answer;
+
+    const checkAnswer = (text) => {
+      if (text === answer) {
+        setCorrectAnswer("correct");
+        setButtonText("正解！！");
+        setButtonColor("#BDB2FF");
+      } else {
+        setCorrectAnswer("wrong");
+        setCorrectAnswer("");
+        return Alert.alert("不正解！もう1度！");
+      }
+    };
+
     return (
       <>
         <BodyText>Please write this in {selectedDialect.name_en}:</BodyText>
@@ -22,8 +40,10 @@ export const Quiz = ({ selectedDialect, grammar }) => {
         <ButtonContainer>
           <Button
             mode="contained"
-            onPress={() => console.log("button pressed")}
-            color="#40BA62"
+            onPress={() => {
+              checkAnswer(text);
+            }}
+            color={buttonColor}
             labelStyle={{ color: "#fff", fontSize: 18 }}
             style={{
               width: "50%",
@@ -31,7 +51,7 @@ export const Quiz = ({ selectedDialect, grammar }) => {
               justifyContent: "center",
             }}
           >
-            答え合わせ
+            {buttonText}
           </Button>
         </ButtonContainer>
       </>
@@ -44,7 +64,7 @@ export const Quiz = ({ selectedDialect, grammar }) => {
 const BodyText = styled.Text`
   text-align: center;
   font-size: 18px;
-  margin: 5px;
+  margin: 8px;
   font-weight: bold;
 `;
 
