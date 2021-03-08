fe8dbbe4b873e5e8b3cbb6531326df50c26da6d3
Chisato Murakami <kiwipom1031@gmail.com>
2021-03-08 11:32:00 -0800

    Add signup method with email, password, and password comfirmation.

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components/native";
 import { TextInput, Button } from "react-native-paper";
 
 export const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConf, setPasswordConf] = useState("");
+
+  const signup = (email, password, passwordConf) => {
+    console.log(email, password, passwordConf);
+  };
+
   return (
     <RegistrationContainer>
       <TextHeader>Sign up</TextHeader>
       <FormContainer>
-        <TextInput label="Email" style={{ margin: 20 }}></TextInput>
-        <TextInput label="Password" style={{ margin: 20 }}></TextInput>
+        <TextInput
+          label="Email"
+          style={{ margin: 20 }}
+          autoCapitalize="none"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        ></TextInput>
+        <TextInput
+          label="Password"
+          style={{ margin: 20 }}
+          autoCapitalize="none"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        ></TextInput>
+        <TextInput
+          label="Password Confirmation"
+          style={{ margin: 20 }}
+          autoCapitalize="none"
+          value={passwordConf}
+          onChangeText={(text) => setPasswordConf(text)}
+        ></TextInput>
         <ButtonContainer>
           <Button
             mode="contained"
@@ -23,6 +50,8 @@ export const Signup = () => {
               margin: 10,
               justifyContent: "center",
             }}
+            type="submit"
+            onPress={() => signup(email, password, passwordConf)}
           >
             Sign up
           </Button>
