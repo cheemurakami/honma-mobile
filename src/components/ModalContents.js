3a8e67ca566b0d7f449f169414dc40ea6f19b324
Chisato Murakami <kiwipom1031@gmail.com>
2021-12-29 15:04:50 -0800

    display description and default image

--- src/components/ModalContents.js
+++ src/components/ModalContents.js
@@ -1,14 +1,24 @@
 import React from "react";
 import styled from "styled-components/native";
+import { Image } from 'react-native';
 
 export const ModalContents = ({ selectedDialect }) => {
+
+  const imageUrl = selectedDialect.default_image
 
   return (
     <>
       <ModalContainer opacity={1}>
         <ModalHeader>
-          {selectedDialect.name_jp} {selectedDialect.name_en}
+          {selectedDialect.name_jp} {selectedDialect.name_en} 
         </ModalHeader>
+        <ModalBody>
+          {selectedDialect.description}
+        </ModalBody>
+        <Image
+          style={{width: 200, height: 200}}
+          source={{uri: imageUrl}}
+        />
       </ModalContainer>
       </>
   );
@@ -27,6 +37,15 @@ const ModalHeader = styled.Text`
   color: black;
   font-weight: bold;
   font-size: 20px;
+`;
+
+const ModalBody = styled.Text`
+  color: black;
+  font-size: 16px;
+`;
+
+const ModalBodyImage = styled.Image`
+  background-color: white;
 `;
 
 export default ModalContents;
