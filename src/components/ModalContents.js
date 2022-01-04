700f7a068f3e1aa578ca647e1d6bebde882a283d
Chisato Murakami <kiwipom1031@gmail.com>
2022-01-03 21:56:11 -0800

    add styling for modal and mock category buttons

--- src/components/ModalContents.js
+++ src/components/ModalContents.js
@@ -1,25 +1,33 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Image } from 'react-native';
 
 export const ModalContents = ({ selectedDialect }) => {
-
-  const imageUrl = selectedDialect.default_image
+  const imageUrl = selectedDialect.default_image;
 
   return (
     <>
       <ModalContainer opacity={1}>
         <ModalHeader>
-          {selectedDialect.name_jp} {selectedDialect.name_en} 
+          {selectedDialect.name_jp} {selectedDialect.name_en}
         </ModalHeader>
-        <ModalBodyImage
-          source={{uri: imageUrl}}
-        />
-        <ModalBody>
-          {selectedDialect.description}
-        </ModalBody>
+        <ModalBodyImage source={{ uri: imageUrl }} />
+        <ModalBody>{selectedDialect.description}</ModalBody>
+        <ButtonContainer>
+          <CategoryButton>
+            <CategoryButtonText>Restaurant</CategoryButtonText>
+          </CategoryButton>
+          <CategoryButton>
+            <CategoryButtonText>Museum</CategoryButtonText>
+          </CategoryButton>
+          <CategoryButton>
+            <CategoryButtonText>Scenery</CategoryButtonText>
+          </CategoryButton>
+          <CategoryButton>
+            <CategoryButtonText>Districts</CategoryButtonText>
+          </CategoryButton>
+        </ButtonContainer>
       </ModalContainer>
-      </>
+    </>
   );
 };
 
@@ -29,26 +37,53 @@ const ModalContainer = styled.View`
   justify-content: center;
   align-items: center;
   width: 300px;
-  height: 80%;
+  height: 85%;
   border-radius: 25px;
+  margin-top: 0;
+  margin-bottom: 20%;
 `;
+
 const ModalHeader = styled.Text`
   color: black;
   font-weight: bold;
   font-size: 20px;
 `;
 
-const ModalBody = styled.Text`
-  color: black;
-  font-size: 16px;
-  margin: 16px; 
-`;
-
 const ModalBodyImage = styled.Image`
   background-color: white;
   width: 200px;
   height: 200px;
-  margin: 16px; 
+  margin: 16px;
+`;
+
+const ModalBody = styled.Text`
+  color: black;
+  font-size: 16px;
+  margin: 16px;
+`;
+
+const ButtonContainer = styled.View`
+  background-color: #fff;
+  justify-content: center;
+  align-items: center;
+  width: 280px;
+`;
+
+const CategoryButton = styled.TouchableOpacity`
+  margin-bottom: 10px;
+  border-radius: 5px;
+  width: 180px;
+  height: 30px;
+  justify-content: center;
+  background-color: #ffe45e;
+  background-color: ${() => "#f5cc00"};
+`;
+
+const CategoryButtonText = styled.Text`
+  text-align: center;
+  font-weight: bold;
+  font-size: 20px;
+  color: #fff;
 `;
 
 export default ModalContents;
