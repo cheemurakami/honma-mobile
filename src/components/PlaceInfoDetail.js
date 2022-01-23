91569a48f9af0fc8c82ef31023b5059fe5319a1f
Chisato Murakami <kiwipom1031@gmail.com>
2022-01-22 18:18:25 -0800

    SDK update and create a detail page file

---
+++ src/components/PlaceInfoDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+export const PlaceInfoDetail = (props) => {
+  return <div>PlaceInfoDetailPage</div>;
+};
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PlaceInfoDetail);
